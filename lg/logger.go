6ff2ba2749ae63a1eb2bbf096ed793bd2c4ecb6e
eugeis <eoeisler@gmail.com>
2020-02-04 15:38:49 +0100

    Add logrus helper

--- lg/logger.go
+++ lg/logger.go
@@ -1,47 +1,12 @@
 package lg
 
 import (
-	"log"
-	"os"
+	"github.com/sirupsen/logrus"
 )
 
-var Debug = true
-var Info = true
-var Err = true
-
-func NewLogger(prefix string) *DebugLogger {
-	return &DebugLogger{Log: log.New(os.Stdout, prefix, log.LstdFlags)}
-}
-
-type DebugLogger struct {
-	Log *log.Logger
-}
-
-func (l *DebugLogger) IsDebugging() bool {
-	return Debug
-}
-
-func (l *DebugLogger) IsInfo() bool {
-	return l.IsDebugging() || Info
-}
-func (l *DebugLogger) IsError() bool {
-	return l.IsInfo() || Err
-}
-
-func (l *DebugLogger) Debug(format string, values ...interface{}) {
-	if l.IsDebugging() {
-		l.Log.Printf(format, values...)
-	}
-}
-
-func (l *DebugLogger) Info(format string, values ...interface{}) {
-	if l.IsInfo() {
-		l.Log.Printf(format, values...)
-	}
-}
-
-func (l *DebugLogger) Err(format string, values ...interface{}) {
-	if l.IsError() {
-		l.Log.Printf(format, values...)
-	}
+func LogrusTimeAsTimestampFormatter() {
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "15:04:05"
+	customFormatter.FullTimestamp = true
+	logrus.SetFormatter(customFormatter)
 }
