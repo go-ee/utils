2e6b5bc6cfc068cc2daf75afb843979fcd2db7c0
eugeis <eoeisler@gmail.com>
2022-06-17 10:02:53 +0200

    feat: extend logging utils

--- lg/logger.go
+++ lg/logger.go
@@ -2,6 +2,10 @@ package lg
 
 import (
 	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func LogrusTimeAsTimestampFormatter() {
@@ -9,4 +13,22 @@ func LogrusTimeAsTimestampFormatter() {
 	customFormatter.TimestampFormat = "15:04:05"
 	customFormatter.FullTimestamp = true
 	logrus.SetFormatter(customFormatter)
+}
+
+func LogrusToFile(appName string, folder string) {
+	runID := time.Now().Format("_2006-01-02-15-04-05")
+	logLocation := filepath.Join(folder, appName+runID+".log")
+	logFile, err := os.OpenFile(logLocation, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		logrus.Fatalf("Failed to open log file %s for output: %s", logLocation, err)
+	}
+	logrus.SetOutput(io.MultiWriter(logFile))
+	logrus.RegisterExitHandler(func() {
+		if logFile == nil {
+			return
+		}
+		logFile.Close()
+	})
+	logrus.WithFields(logrus.Fields{"at": "start", "log-location": logLocation}).Info()
+	// perform actions
 }
