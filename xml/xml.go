889a01c9d97d6c2e35e10e7bfdee7c85c3b7d8c0
Eugen Eisler <eugen.eisler@siemens.com>
2023-09-29 11:04:42 +0200

    implement reflect resolveFirst and xml attr

--- xml/xml.go
+++ xml/xml.go
@@ -4,7 +4,17 @@ import (
 	"encoding/xml"
 )
 
-func FindAttr(attrs []xml.Attr, space, local string) (ret *xml.Attr) {
+func FindAttrForLocal(attrs []xml.Attr, local string) (ret *xml.Attr) {
+	for _, attr := range attrs {
+		if attr.Name.Local == local {
+			ret = &attr
+			break
+		}
+	}
+	return
+}
+
+func FindAttrForSpaceAndLocal(attrs []xml.Attr, space, local string) (ret *xml.Attr) {
 	for _, attr := range attrs {
 		if attr.Name.Local == local && attr.Name.Space == space {
 			ret = &attr
