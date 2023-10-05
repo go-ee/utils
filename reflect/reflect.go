a8818b5c1cfa18ad05cf2c18ffc4cbfb76f853a4
Eugen Eisler <eugen.eisler@siemens.com>
2023-10-05 14:54:49 +0200

    Extend Types creator

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -1,46 +1,42 @@
 package reflect
 
-import "reflect"
+type LabeledCreator func(label string) interface{}
 
 type Types struct {
 	Label string
-	Types map[string]reflect.Type
+	Types map[string]LabeledCreator
 }
 
-func (o *Types) NewInstance(name string) (ret interface{}) {
-	return reflect.New(o.Resolve(name)).Elem().Interface()
-}
-
-func (o *Types) Resolve(name string) reflect.Type {
+func (o *Types) Resolve(name string) LabeledCreator {
 	return o.Types[name]
 }
 
-func (o *Types) Register(name string, typedNil interface{}) {
-	o.Types[name] = reflect.TypeOf(typedNil).Elem()
+func (o *Types) Register(name string, creator LabeledCreator) {
+	o.Types[name] = creator
 }
 
 type LabeledTypes struct {
 	LabeledTypes      map[string]*Types
-	CacheResolveFirst map[string]reflect.Type
+	CacheResolveFirst map[string]LabeledCreator
 }
 
 func (o *LabeledTypes) Register(label string) (ret *Types) {
 	ret = &Types{
 		Label: label,
-		Types: make(map[string]reflect.Type),
+		Types: make(map[string]LabeledCreator),
 	}
 	o.LabeledTypes[label] = ret
 	return
 }
 
-func (o *LabeledTypes) Resolve(namespace, typ string) (ret reflect.Type) {
+func (o *LabeledTypes) Resolve(namespace, typ string) (ret LabeledCreator) {
 	if namespaceTypes := o.LabeledTypes[namespace]; namespaceTypes != nil {
 		ret = namespaceTypes.Resolve(typ)
 	}
 	return
 }
 
-func (o *LabeledTypes) ResolveFirst(typ string) (ret reflect.Type) {
+func (o *LabeledTypes) ResolveFirst(typ string) (ret LabeledCreator) {
 	if o.CacheResolveFirst != nil {
 		var ok bool
 		if ret, ok = o.CacheResolveFirst[typ]; !ok {
@@ -53,7 +49,7 @@ func (o *LabeledTypes) ResolveFirst(typ string) (ret reflect.Type) {
 	return
 }
 
-func (o *LabeledTypes) resolveFirst(typ string) (ret reflect.Type) {
+func (o *LabeledTypes) resolveFirst(typ string) (ret LabeledCreator) {
 	for _, types := range o.LabeledTypes {
 		ret = types.Resolve(typ)
 		if ret != nil {
